85a87f0bccf58fa0d2b2ddc84332b7a1faa53913
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2022-11-09 22:35:04 +0800

    create group chat socket

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         io.emit('message', 'A user has left the chat');
     });
+    
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', msg);
+    });
 
 });
 
