f30f7b2a64f7a02a42b96df4d3221b28863f26ef
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2022-11-09 21:17:22 +0800

    adding IO

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
+// Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when client connects
+io.on('connection', (socket) => {
+    console.log('New WS Connections');
+});
 
 const PORT = 3000 || process.env.PORT;
 
