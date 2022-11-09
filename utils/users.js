9c3983bce9012bbcdf01f770a9a539fd9a0604e9
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2022-11-09 23:13:57 +0800

    create utility for user leave and getroomuser

--- utils/users.js
+++ utils/users.js
@@ -15,7 +15,22 @@ function getCurrentUser(id) {
     return users.find(user => user.id == id);
 }
 
+// User leaves chat
+function userLeave(id) {
+    const index = users.findIndex(user => user.id === id);
+
+    if (index !== -1) {
+        return users.splice(index, 1);
+    }
+}
+
+function getRoomUsers(room) {
+    return users.filter(user => user.room === room);
+}
+
 module.exports = {
     userJoin,
     getCurrentUser,
+    userLeave,
+    getRoomUsers,
 }
